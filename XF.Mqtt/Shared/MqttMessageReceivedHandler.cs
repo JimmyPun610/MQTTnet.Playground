e45f6d23bcb526c8f03f8b065b3d50005f6b8523
Jimmy Pun <11005408@life.hkbu.edu.hk>
2020-06-08 13:40:55 +0800

    Add files via upload

---
+++ XF.Mqtt/Shared/MqttMessageReceivedHandler.cs
@@ -0,0 +1,22 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Receiving;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.Mqtt
+{
+    internal class MqttMessageReceivedHandler : IMqttApplicationMessageReceivedHandler
+    {
+        Action<MqttApplicationMessageReceivedEventArgs> _messageReceived;
+        public MqttMessageReceivedHandler(Action<MqttApplicationMessageReceivedEventArgs> messageReceived)
+        {
+            _messageReceived = messageReceived;
+        }
+        public async Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs)
+        {
+            _messageReceived?.Invoke(eventArgs);
+        }
+    }
+}
