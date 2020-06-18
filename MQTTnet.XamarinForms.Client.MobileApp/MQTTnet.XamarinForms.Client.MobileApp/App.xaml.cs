6dd2decca22070bbb1ca8953d35767f84566181b
Jimmy Pun <11005408@life.hkbu.edu.hk>
2020-06-18 21:32:25 +0800

    Update App.xaml.cs

--- MQTTnet.XamarinForms.Client.MobileApp/MQTTnet.XamarinForms.Client.MobileApp/App.xaml.cs
+++ MQTTnet.XamarinForms.Client.MobileApp/MQTTnet.XamarinForms.Client.MobileApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace MQTTnet.XamarinForms.Client.MobileApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
